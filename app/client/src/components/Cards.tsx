dbef5532cfb5199b84acba32fd8cf0b074971839
John Christian Vicente <johnc.vicente1@gmail.com>
2025-12-03 14:39:21 +0800

    Refactor for better visual

--- app/client/src/components/Cards.tsx
+++ app/client/src/components/Cards.tsx
@@ -1,13 +1,12 @@
-import { IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent } from '@ionic/react';
-
+import { IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle } from '@ionic/react';
 
 function PlainCard({ title, content, color }: { title: string, content: string, color: string }) {
     return (
         <IonCard mode="ios" color={color}>
             <IonCardHeader>
+                <IonCardSubtitle>{content}</IonCardSubtitle>
                 <IonCardTitle>{title}</IonCardTitle>
             </IonCardHeader>
-            <IonCardContent>{content}</IonCardContent>
         </IonCard>
     );
 }

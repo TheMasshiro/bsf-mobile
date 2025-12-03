fc5863aae91cb1cb3c56710e1c6618596cb1cbad
John Christian Vicente <johnc.vicente1@gmail.com>
2025-12-03 14:39:56 +0800

    Added toast to alert users

--- app/client/src/components/RadioButton.tsx
+++ app/client/src/components/RadioButton.tsx
@@ -1,5 +1,6 @@
-import { IonCard, IonCardContent, IonCardTitle, IonCardHeader, IonItem, IonList, IonRadio, IonRadioGroup, IonCardSubtitle } from '@ionic/react';
+import { IonCard, IonCardContent, IonCardTitle, IonCardHeader, IonItem, IonList, IonRadio, IonRadioGroup, IonCardSubtitle, useIonToast } from '@ionic/react';
 import "./RadioButton.css"
+import { secondsToTime } from '../utils/convertToTime';
 
 interface Time {
     id: number;
@@ -7,45 +8,59 @@ interface Time {
     seconds: number;
 }
 
-const times: Time[] = [
-    {
-        id: 1,
-        name: '0 Hour',
-        seconds: 0,
-    },
-    {
-        id: 2,
-        name: '8 Hours',
-        seconds: 28800,
-    },
-    {
-        id: 3,
-        name: '12 Hours',
-        seconds: 43200,
-    },
-];
 
-const compareWith = (o1: Time, o2: Time) => {
-    return o1.id === o2.id;
-};
+
 
 function TimeSelection() {
+    const compareWith = (o1: Time, o2: Time) => {
+        return o1.id === o2.id;
+    };
+
+    const times: Time[] = [
+        {
+            id: 1,
+            name: '0 Hour',
+            seconds: 0,
+        },
+        {
+            id: 2,
+            name: '8 Hours',
+            seconds: 28800,
+        },
+        {
+            id: 3,
+            name: '12 Hours',
+            seconds: 43200,
+        },
+    ];
+    const [present] = useIonToast()
+    const presentToast = (message: string) => {
+        present({
+            message: message,
+            duration: 1500,
+            position: "top",
+            mode: "ios",
+            layout: "stacked",
+            swipeGesture: "vertical",
+        })
+    }
     return (
         <IonCard mode="ios">
             <IonCardHeader>
-                <IonCardTitle>Select Time:</IonCardTitle>
                 <IonCardSubtitle>Set time for light</IonCardSubtitle>
+                <IonCardTitle>Select Time:</IonCardTitle>
             </IonCardHeader>
             <IonCardContent>
-                <IonList mode="ios">
+                <IonList>
                     <IonRadioGroup
                         value={times[0]}
                         compareWith={compareWith}
-                        onIonChange={(event) => console.log('Current value:', JSON.stringify(event.detail.value))}
+                        onIonChange={(event) => {
+                            presentToast(event.detail.value.seconds ? `Light timer set to ${secondsToTime(event.detail.value.seconds)}` : `Light timer disabled`)
+                        }}
                     >
-
                         {times.map((time) => (
-                            <IonItem>
+                            <IonItem key={time.id}>
                                 <IonRadio key={time.id} value={time}>
                                     {time.name}
                                 </IonRadio>

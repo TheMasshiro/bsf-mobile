e5542bfd0c591dadf11c6d262bcf259576ccff8f
John Christian Vicente <johnc.vicente1@gmail.com>
2025-12-05 02:19:24 +0800

    Initialize SQLite

--- app/client/src/App.tsx
+++ app/client/src/App.tsx
@@ -51,68 +51,86 @@ import './theme/variables.css';
 import { LifeCycleButton } from './components/AppActionButtons/ActionButtons';
 import { LifeCycleProvider } from './context/LifeCycleContext';
 import { Capacitor } from '@capacitor/core';
+import { createContext } from 'react';
+import SqliteService from './services/sqliteService';
+import DbVersionService from './services/dbVersionService';
+import StorageService from './services/storageService';
+import AppInitializer from './components/AppInitializer/AppInitializer';
 
 export const platform = Capacitor.getPlatform();
+
+// Singleton Services
+export const SqliteServiceContext = createContext(SqliteService)
+export const DbVersionServiceContext = createContext(DbVersionService)
+export const StorageServiceContext = createContext(new StorageService(SqliteService, DbVersionService))
 
 setupIonicReact();
 
 const App: React.FC = () => {
     return (
-        <IonApp>
-            <LifeCycleProvider>
-                <LifeCycleButton />
-                <IonReactRouter>
-                    <IonTabs>
-                        <IonRouterOutlet>
-                            <Route exact path="/sensors">
-                                <SensorsPage />
-                            </Route>
-                            <Route exact path="/controls">
-                                <ControlsPage />
-                            </Route>
-                            <Route exact path="/notifications">
-                                <NotificationsPage />
-                            </Route>
-                            <Route path="/analytics">
-                                <AnalyticsPage />
-                            </Route>
-                            <Route path="/menu">
-                                <MenuPage />
-                            </Route>
-                            <Route exact path="/">
-                                <Redirect to="/sensors" />
-                            </Route>
-                        </IonRouterOutlet>
-                        <IonTabBar slot="bottom">
-                            <IonTabButton tab="sensors" href="/sensors">
-                                <IonIcon aria-hidden="true" icon={thermometerOutline} />
-                                <IonLabel>Sensors</IonLabel>
-                            </IonTabButton>
+        <SqliteServiceContext.Provider value={SqliteService}>
+            <DbVersionServiceContext.Provider value={DbVersionService}>
+                <StorageServiceContext.Provider value={new StorageService(SqliteService, DbVersionService)}>
+                    <AppInitializer>
+                        <IonApp>
+                            <LifeCycleProvider>
+                                <LifeCycleButton />
+                                <IonReactRouter>
+                                    <IonTabs>
+                                        <IonRouterOutlet>
+                                            <Route exact path="/sensors">
+                                                <SensorsPage />
+                                            </Route>
+                                            <Route exact path="/controls">
+                                                <ControlsPage />
+                                            </Route>
+                                            <Route exact path="/notifications">
+                                                <NotificationsPage />
+                                            </Route>
+                                            <Route path="/analytics">
+                                                <AnalyticsPage />
+                                            </Route>
+                                            <Route path="/menu">
+                                                <MenuPage />
+                                            </Route>
+                                            <Route exact path="/">
+                                                <Redirect to="/sensors" />
+                                            </Route>
+                                        </IonRouterOutlet>
+                                        <IonTabBar slot="bottom">
+                                            <IonTabButton tab="sensors" href="/sensors">
+                                                <IonIcon aria-hidden="true" icon={thermometerOutline} />
+                                                <IonLabel>Sensors</IonLabel>
+                                            </IonTabButton>
 
-                            <IonTabButton tab="controls" href="/controls">
-                                <IonIcon aria-hidden="true" icon={toggleOutline} />
-                                <IonLabel>Controls</IonLabel>
-                            </IonTabButton>
+                                            <IonTabButton tab="controls" href="/controls">
+                                                <IonIcon aria-hidden="true" icon={toggleOutline} />
+                                                <IonLabel>Controls</IonLabel>
+                                            </IonTabButton>
 
-                            <IonTabButton tab="analytics" href="/analytics">
-                                <IonIcon aria-hidden="true" icon={analyticsOutline} />
-                                <IonLabel>Analytics</IonLabel>
-                            </IonTabButton>
+                                            <IonTabButton tab="analytics" href="/analytics">
+                                                <IonIcon aria-hidden="true" icon={analyticsOutline} />
+                                                <IonLabel>Analytics</IonLabel>
+                                            </IonTabButton>
 
-                            <IonTabButton tab="notifications" href="/notifications">
-                                <IonIcon aria-hidden="true" icon={notificationsCircleOutline} />
-                                <IonLabel>Notifications</IonLabel>
-                            </IonTabButton>
+                                            <IonTabButton tab="notifications" href="/notifications">
+                                                <IonIcon aria-hidden="true" icon={notificationsCircleOutline} />
+                                                <IonLabel>Notifications</IonLabel>
+                                            </IonTabButton>
 
-                            <IonTabButton tab="menu" href="/menu">
-                                <IonIcon aria-hidden="true" icon={menuOutline} />
-                                <IonLabel>More</IonLabel>
-                            </IonTabButton>
-                        </IonTabBar>
-                    </IonTabs>
-                </IonReactRouter>
-            </LifeCycleProvider>
-        </IonApp>
+                                            <IonTabButton tab="menu" href="/menu">
+                                                <IonIcon aria-hidden="true" icon={menuOutline} />
+                                                <IonLabel>Menu</IonLabel>
+                                            </IonTabButton>
+                                        </IonTabBar>
+                                    </IonTabs>
+                                </IonReactRouter>
+                            </LifeCycleProvider>
+                        </IonApp>
+                    </AppInitializer>
+                </StorageServiceContext.Provider>
+            </DbVersionServiceContext.Provider>
+        </SqliteServiceContext.Provider>
     );
 };
 

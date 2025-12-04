2673c17554ff19081e0dae3efc5d092373d0f03c
John Christian Vicente <johnc.vicente1@gmail.com>
2025-12-04 16:33:28 +0800

    Added channel and fixed notification delay

--- app/client/src/utils/localNotification.ts
+++ app/client/src/utils/localNotification.ts
@@ -1,15 +1,29 @@
 import { LocalNotifications } from "@capacitor/local-notifications";
 
-export const actuatorNotification = async ({ body }: { body: string }) => {
-  await LocalNotifications.schedule({
-    notifications: [
-      {
-        title: "BSF Mobile",
-        body: body,
-        id: Math.ceil(Math.random() * 100),
-        schedule: { at: new Date(Date.now() + 1000 * 5) },
-        ongoing: false,
-      },
-    ],
-  });
-};
+export async function createActuatorNotifications(body: string) {
+  const check = await LocalNotifications.checkPermissions();
+  const res = await LocalNotifications.requestPermissions();
+  if (res.display === "granted") {
+    LocalNotifications.createChannel({
+      id: "1",
+      name: "Actuators Alert",
+      description: "Alerts for turning actuators on/off.",
+    }).then(() =>
+      LocalNotifications.schedule({
+        notifications: [
+          {
+            channelId: "1",
+            id: 1,
+            title: `BSF Mobile`,
+            body: body,
+            schedule: {
+              at: new Date(Date.now() + 1000),
+              allowWhileIdle: true,
+            },
+            ongoing: false,
+          },
+        ],
+      }),
+    );
+  }
+}
